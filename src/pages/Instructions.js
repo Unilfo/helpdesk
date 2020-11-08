5df4a591a83cb7405fbaf0faf97c8748592a4e7d
Unilfo <kokopal13@gmail.com>
2020-11-08 23:03:19 +0300

    2

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -1,14 +1,15 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import Title from '../components/Title';
-import makeStyles from '@material-ui/core/styles/makeStyles'
-import TreeView from '@material-ui/lab/TreeView';
+import Grid from '@material-ui/core/Grid'
+import {Input} from '@material-ui/core'
+import TreeView from '@material-ui/lab/TreeView'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TreeItem from '@material-ui/lab/TreeItem';
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-import {Input} from '@material-ui/core'
-
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import { Page } from 'react-pdf';
+import { Document } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 const useStyles = makeStyles({
   root: {
@@ -19,18 +20,26 @@ const useStyles = makeStyles({
 });
 
 export default function Instructions() {
-
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState([]);
-  const [selected, setSelected] = React.useState([]);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  const handleToggle = (event, nodeIds) => {
-    setExpanded(nodeIds);
-  };
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
 
-  const handleSelect = (event, nodeIds) => {
-    setSelected(nodeIds);
-  };
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
 
   return (
     <Fragment>
@@ -42,65 +51,57 @@ export default function Instructions() {
           <Input placeholder={'Поиск'}></Input>
         </Grid>
       </Grid>
-
-      <TreeView
-        className={classes.root}
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-        expanded={expanded}
-        selected={selected}
-        onNodeToggle={handleToggle}
-        onNodeSelect={handleSelect}
-      >
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="5" label="Documents">
-          <TreeItem nodeId="6" label="Material-UI">
-            <TreeItem nodeId="7" label="src">
-              <TreeItem nodeId="8" label="index.js" />
-              <TreeItem nodeId="9" label="tree-view.js" />
-            </TreeItem>
-          </TreeItem>
-        </TreeItem>
-      </TreeView>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+            <TreeView
+              className={classes.root}
+              defaultCollapseIcon={<ExpandMoreIcon />}
+              defaultExpandIcon={<ChevronRightIcon />}
+              multiSelect
+            >
+              <TreeItem nodeId="1" label="Applications">
+                <TreeItem nodeId="2" label="Calendar" />
+                <TreeItem nodeId="3" label="Chrome" />
+                <TreeItem nodeId="4" label="Webstorm" />
+              </TreeItem>
+              <TreeItem nodeId="5" label="Documents">
+                <TreeItem nodeId="6" label="Material-UI">
+                  <TreeItem nodeId="7" label="src">
+                    <TreeItem nodeId="8" label="index.js" />
+                    <TreeItem nodeId="9" label="tree-view.js" />
+                  </TreeItem>
+                </TreeItem>
+              </TreeItem>
+            </TreeView>
+          </Grid>
+          <Grid item xs={6}>
+            <Document
+              file="Kak_ustroen_JavaScript_2019_Krokford.pdf"
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <div>
+              <p>
+                Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+              </p>
+              <button
+                type="button"
+                disabled={pageNumber <= 1}
+                onClick={previousPage}
+              >
+                Previous
+              </button>
+              <button
+                type="button"
+                disabled={pageNumber >= numPages}
+                onClick={nextPage}
+              >
+                Next
+              </button>
+            </div>
+          </Grid>
+        </Grid>
     </Fragment>
   );
 }
