f74f01d93eacb7b5de977dacabac2b08f058d1e6
Unilfo <kokopal13@gmail.com>
2020-11-22 15:56:12 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Login from './pages/Login/Login'
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <Dashboard/>
     </div>
   );
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,128 @@
+import React, {useState} from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  error:{
+    marginTop: 15,
+    color: 'red'
+  }
+}))
+
+export default function SignInSide() {
+  const classes = useStyles()
+  const [login, setlogin] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
+
+  const changeLogin = (e) => {
+    setlogin(e.target.value)
+  }
+
+  const changePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const Error = () => {
+    if(error){
+      return(
+        <div className={classes.error}>
+          Неверный логин или пароль
+        </div>
+      )
+    }
+    return null
+  }
+
+
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Help desk
+          </Typography>
+          <Error/>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Логин"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={login}
+              onChange={changeLogin}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Пароль"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={changePassword}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Войти
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  )
+}
