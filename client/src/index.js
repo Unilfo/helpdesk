0fb4bb49bf11bef1105ea8b4dc5829cb208687a6
Unilfo <kokopal13@gmail.com>
2020-12-01 23:49:00 +0300

    1

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'fontsource-roboto'
+import {ApolloClient, InMemoryCache} from '@apollo/client'
+import {ApolloProvider} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
 
-reportWebVitals();
+reportWebVitals()
--- client/src/pages/Employees/Employees.js
+++ client/src/pages/Employees/Employees.js
@@ -13,96 +13,41 @@ import makeStyles from '@material-ui/core/styles/makeStyles'
 import ModalForm from './Modal'
 import {Input} from '@material-ui/core'
 import Button from '@material-ui/core/Button'
+import {useQuery, gql} from '@apollo/client'
 
+const GetAllUsers = gql`
+    query GetAllUsers{
+        users{
+            id
+            name
+            patronymic
+            surname
+            roleId{
+                title
+            }
+            statusId{
+                title
+            }
+            tab_number
+            login
+            password
+        }
+    }
+`
 
 
 const columns = [
-  {id: 'id', label: '№', minWidth: 80,},
-  {id: 'fio1', label: 'Фамилия', minWidth: 150, maxWidth: 250, align: 'left',},
-  {id: 'fio2', label: 'Имя', minWidth: 150, maxWidth: 250, align: 'left',},
-  {id: 'fio3', label: 'Отчество', minWidth: 150, maxWidth: 250, align: 'left',},
-  {id: 'status', label: 'Статус', minWidth: 150, maxWidth: 250, align: 'left',},
-  {id: 'role', label: 'Роль', minWidth: 170, maxWidth: 250, align: 'left',},
-  {id: 'tabNumber', label: 'Табельный номер', minWidth: 100, maxWidth: 250, align: 'left',},
-  {id: 'login', label: 'login', minWidth: 100, maxWidth: 250, align: 'left',},
-  {id: 'password', label: 'password', minWidth: 100, maxWidth: 250, align: 'left',},
+  {id: 'id', label: '№', minWidth: 80},
+  {id: 'surname', label: 'Фамилия', minWidth: 150, maxWidth: 250, align: 'left'},
+  {id: 'name', label: 'Имя', minWidth: 150, maxWidth: 250, align: 'left'},
+  {id: 'patronymic', label: 'Отчество', minWidth: 150, maxWidth: 250, align: 'left'},
+  {id: 'statusId', label: 'Статус', minWidth: 150, maxWidth: 250, align: 'left'},
+  {id: 'roleId', label: 'Роль', minWidth: 170, maxWidth: 250, align: 'left'},
+  {id: 'tab_number', label: 'Табельный номер', minWidth: 100, maxWidth: 250, align: 'left'},
+  {id: 'login', label: 'login', minWidth: 100, maxWidth: 250, align: 'left'},
+  {id: 'password', label: 'password', minWidth: 100, maxWidth: 250, align: 'left'},
 ]
 
-function createData(id, fio1, fio2, fio3, status, role, tabNumber, login, password) {
-  return {id, fio1, fio2, fio3, status, role, tabNumber, login, password}
-}
-
-
-const rows = [
-  createData(1, 'Иванов', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '12', 'pog', 'qwe'),
-  createData(2, 'Иванов', 'Иван', 'Иванович', {id: 2, label: 'Неактивен'}, {
-    id: 1,
-    label: 'Администратор'
-  }, '34', 'login', 'qwe'),
-  createData(3, 'Иванов', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '123', 'login', 'qwe'),
-  createData(4, 'Иванов', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '96', 'login', 'qwe'),
-  createData(5, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 1,
-    label: 'Администратор'
-  }, '0', 'login', 'qwe'),
-  createData(6, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(7, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(8, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(9, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(10, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(11, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(12, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(13, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(14, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(15, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(16, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-  createData(17, 'петров', 'Иван', 'Иванович', {id: 1, label: 'Действует'}, {
-    id: 2,
-    label: 'Пользователь'
-  }, '0', 'login', 'qwe'),
-]
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -114,30 +59,45 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     [theme.breakpoints.down('sm')]: {
-      display:'none'
+      display: 'none',
     },
   },
   input_search: {
-    marginBottom: 15
+    marginBottom: 15,
   },
   caption: {
     [theme.breakpoints.up('xs')]: {
-      display:'none'
+      display: 'none',
     },
   },
-  button_open:{
-
-  },
+  button_open: {},
 }))
 
 export default function Employees() {
+  const {loading, error, data} = useQuery(GetAllUsers)
   const classes = useStyles()
   const [page, setPage] = React.useState(0)
   const [rowsPerPage, setRowsPerPage] = React.useState(10)
   const [open, setOpen] = React.useState(false)
   const [searchText, SetSearchText] = useState(null)
-  const [dataT, setData] = useState(rows)
+  const [dataT, setData] = useState('')
   const [item, setItem] = useState({})
+
+  // useEffect(()=>{
+  //   setData(data)
+  //   console.log(dataT)
+  // },[data])
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+  if (error) {
+    return <p>Error </p>
+  }
+
+  if (data) {
+    // console.log(data)
+  }
 
 
   const handleChangePage = (event, newPage) => {
@@ -166,7 +126,7 @@ export default function Employees() {
         status: {id: '', label: ''},
         tabNumber: {},
         login: '',
-        password: ''
+        password: '',
       }
       setItem(itemNull)
     }
@@ -177,27 +137,25 @@ export default function Employees() {
     SetSearchText(event.target.value)
   }
 
-  useEffect(() => {
-    if (searchText == null)
-      return rows
-    else {
-      const filteredRowsFIO = rows.filter((data) => {
-        return data.fio1.toLowerCase().includes(searchText.toLowerCase().trim())
-      })
-      const filteredRowsTabelNumber = rows.filter((data) => {
-        return data.tabNumber.toLowerCase().includes(searchText.toLowerCase().trim())
-      })
-      const filteredRowsByLogin = rows.filter((data) => {
-        return data.login.toLowerCase().includes(searchText.toLowerCase().trim())
-      })
-      const filteredRows = [...new Set([...filteredRowsFIO, ...filteredRowsTabelNumber, ...filteredRowsByLogin])]
-      setData(filteredRows)
-    }
-  }, [searchText])
+  // useEffect(() => {
+  //   if (searchText == null)
+  //     return data
+  //   else {
+  //     const filteredRowsFIO = data.filter((data) => {
+  //       return data.fio1.toLowerCase().includes(searchText.toLowerCase().trim())
+  //     })
+  //     const filteredRowsTabelNumber = data.filter((data) => {
+  //       return data.tabNumber.toLowerCase().includes(searchText.toLowerCase().trim())
+  //     })
+  //     const filteredRowsByLogin = data.filter((data) => {
+  //       return data.login.toLowerCase().includes(searchText.toLowerCase().trim())
+  //     })
+  //     const filteredRows = [...new Set([...filteredRowsFIO, ...filteredRowsTabelNumber, ...filteredRowsByLogin])]
+  //     setData(filteredRows)
+  //   }
+  // }, [searchText])
 
-
-
-
+  // {dataT.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
   return (
     <Fragment>
       <Grid container spacing={3}>
@@ -229,11 +187,14 @@ export default function Employees() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {dataT.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              {data.users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                 return (
                   <TableRow hover role="checkbox" tabIndex={-1} key={row.id} onClick={() => openModal(row)}>
                     {columns.map((column) => {
-                      const value = row[column.id].label ? row[column.id].label : row[column.id]
+                      let value = row[column.id]
+                      if(row[column.id] instanceof Object){
+                        value = row[column.id].title
+                      }
                       return (
                         <TableCell key={column.id} align={column.align}>
                           {value}
@@ -249,7 +210,7 @@ export default function Employees() {
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+          count={data.users.length}
           rowsPerPage={rowsPerPage}
           labelRowsPerPage={'Строк на странице'}
           page={page}
